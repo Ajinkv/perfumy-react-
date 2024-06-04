d7d856c5bc5a71a20590b531ce77dc9eaf50bf32
unknown <ajink6384@gmail.com>
2024-06-04 20:06:25 +0300

    update all

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import Navbar from "../components/navbar"
+import Search from "../components/search"
+import Product from "../components/product"
+import About from "../components/about"
+import Contact from "../components/contact"
+
+
+function App(){
+    return(
+        <div>
+    <Navbar></Navbar> 
+    <Search></Search> 
+    <Product></Product>
+    <About></About>
+    <Contact></Contact>
+        </div>
+    )
+}
+export default App
---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+function Navbar(){
+    return(
+  <nav>
+      <><h1>PERFUMY</h1>
+      <ul>
+        <li>HOME |</li>
+        <li>PRODUCT |</li>
+        <li>ABOUT |</li>
+        <li>CONTACT|</li>
+  
+      </ul></>
+      </nav>
+    )
+  }
+  export default Navbar
